a5b844684ecb7d5a60f5fd89f97ab85bfd9ca3a5
Ben Key <54153434+mellophone@users.noreply.github.com>
2025-11-02 16:04:34 -0600

    Test env sharing

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import { Environment } from "@avalon/shared";
 
 export default function Home() {
   return (
@@ -14,7 +15,9 @@ export default function Home() {
           priority
         />
         <div className={styles.intro}>
-          <h1>To get started, edit the page.tsx file.</h1>
+          <h1>
+            To get started, edit the page.tsx file. {Environment.current.port}
+          </h1>
           <p>
             Looking for a starting point or more instructions? Head over to{" "}
             <a
