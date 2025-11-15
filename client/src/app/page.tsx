79fabee334d651d86d4a758ea688ebb881286b06
Drew Pusey <drewpusey654@gmail.com>
2025-11-14 19:50:45 -0600

    First screen

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,70 +1,52 @@
 import Image from "next/image";
-import styles from "./page.module.css";
-import ClientComponent from "./ClientComponent";
 
-export default function Home() {
+const LoginScreen = () => {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className={styles.intro}>
-          <h1>
-            To get started, edit the page.tsx file.
-            <ClientComponent />
-          </h1>
-          <p>
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className={styles.secondary}
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
+    <div>
+      <h1
+        style={{
+          fontSize: "48px",
+          fontWeight: "bold",
+          textAlign: "center",
+          marginTop: "20vh",
+        }}
+      >
+        Avalon
+      </h1>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          position: "absolute",
+          bottom: "10vw",
+          left: "0",
+          right: "0",
+        }}
+      >
+        <button
+          style={{
+            display: "flex",
+            alignItems: "center",
+            padding: "10px 20px",
+            fontSize: "16px",
+            cursor: "pointer",
+            borderRadius: "5px",
+            backgroundColor: "#5865F2",
+            fontFamily: "Discord",
+          }}
+        >
+          <Image
+            src="/Discord-Symbol-White.svg"
+            alt="Discord Logo"
+            width={24}
+            height={24}
+            style={{ marginRight: "16px" }}
+          />
+          Sign in with Discord
+        </button>
+      </div>
     </div>
   );
-}
+};
+
+export default LoginScreen;
