3efb026e46d3e8b41af28a384a8d9217db826542
Ben Key <54153434+mellophone@users.noreply.github.com>
2025-11-17 20:19:40 -0600

    Add socket lifecycle methods

--- server/src/utils/AvalonServer.ts
+++ server/src/utils/AvalonServer.ts
@@ -1,4 +1,4 @@
-import { Server } from "socket.io";
+import { Server, Socket } from "socket.io";
 
 export default class AvalonServer {
   private server: Server;
@@ -6,5 +6,38 @@ export default class AvalonServer {
   constructor(port: number) {
     this.server = new Server(port);
     console.log(`Server live on port: ${port}`);
+
+    this.server.on("connect", this.onConnect);
+
+    process.on("SIGINT", () => {
+      this.server.close();
+      console.log("Server has closed");
+    });
   }
+
+  private onConnect = (socket: Socket): void => {
+    socket.on("disconnect", () => this.onDisconnect(socket));
+
+    const cookieString = socket.handshake.headers.cookie;
+
+    if (!cookieString) {
+      console.log("No cookies found!");
+      return;
+    }
+
+    const cookies = cookieString.split("; ").map((pair) => {
+      const [name, value] = pair.split("=");
+
+      return {
+        name,
+        value,
+      };
+    });
+
+    console.log("Cookies:", cookies);
+  };
+
+  private onDisconnect = (socket: Socket): void => {
+    console.log(`\nThe user left! ${socket.handshake.address}`);
+  };
 }
